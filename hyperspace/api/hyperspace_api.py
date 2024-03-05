b9a76e21ef18136bb98b084da0aafb5c53aeaad8
hyper-space-io <>
2024-03-05 12:43:26 +0000

    HYP-1840 add metadata updates and get doc metadata only (#186)
    * add metadata updates and get doc metadata only
    
    * make metadata_only param optional
    
    * fix python client
    
    ---------
    
    Co-authored-by: Assaf Ohana <148876671+assafohana1982@users.noreply.github.com>

--- hyperspace/api/hyperspace_api.py
+++ hyperspace/api/hyperspace_api.py
@@ -1037,6 +1037,7 @@ class HyperspaceApi(object):
         :param async_req bool
         :param str collection_name: (required)
         :param str document_id: (required)
+        :param bool metadata_only:
         :return: CollectionCollectionNameBody
                  If the method is called asynchronously,
                  returns the request thread.
@@ -1059,12 +1060,13 @@ class HyperspaceApi(object):
         :param async_req bool
         :param str collection_name: (required)
         :param str document_id: (required)
+        :param bool metadata_only:
         :return: CollectionCollectionNameBody
                  If the method is called asynchronously,
                  returns the request thread.
         """
 
-        all_params = ['collection_name', 'document_id']  # noqa: E501
+        all_params = ['collection_name', 'document_id', 'metadata_only']  # noqa: E501
         all_params.append('async_req')
         all_params.append('_return_http_data_only')
         all_params.append('_preload_content')
@@ -1097,6 +1099,8 @@ class HyperspaceApi(object):
         query_params = []
         if 'document_id' in params:
             query_params.append(('documentId', params['document_id']))  # noqa: E501
+        if 'metadata_only' in params:
+            query_params.append(('metadata_only', params['metadata_only']))  # noqa: E501
 
         header_params = {}
 
